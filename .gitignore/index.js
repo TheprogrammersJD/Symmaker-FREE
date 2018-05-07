618f3077e7b51f7fb67431d192e32c318aa41f3b
TheprogrammersJD <solarisfree101@gmail.com>
2018-05-07 07:42:28 -0500

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,45 @@
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  
+  //socket.emit("mapsize", mapsize);
+	
+  socket.on('message name', function(parameter1, parameter2){
+		
+  });
+	
+  
+	setInterval(function(){
+		if(bugsliving < 10000){
+			//socket.emit('bugs', buggeesx[bbuugg], buggeesy[bbuugg], buggeesalive[bbuugg]);
+		}
+	}, 100);
+	
+	
+	
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+	
+  
+});
+
+
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
